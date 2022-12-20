cdbef652212d123a55814ba7e4d072ba1a876307
Gedson <58708778+GEDSONN@users.noreply.github.com>
2022-12-20 04:43:50 +0000

    hh

--- main.sh
+++ main.sh
@@ -2,4 +2,4 @@
 
 #am start --user 0 -n com.android.chrome/com.google.android.apps.chrome.Main https://github.com/MartinCiro/
 
-adb shell am start -a android.intent.action.VIEW
+am start -a android.intent.action.VIEW
