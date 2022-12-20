b10350fe6819423082c5c9bb9a471073a1f3fbe4
Gedson <58708778+GEDSONN@users.noreply.github.com>
2022-12-20 04:50:15 +0000

    ii

--- main.sh
+++ main.sh
@@ -3,3 +3,5 @@
 #am start --user 0 -n com.android.chrome/com.google.android.apps.chrome.Main https://github.com/MartinCiro/
 
 am start -a android.intent.action.VIEW
+
+#oii
---
+++ update.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/GEDSONN/mob.git;
+
+rm main.sh;
+cp -r mob/main.sh main.sh;
+rm -rf mob;
